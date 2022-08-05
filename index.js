6cd7fcc542784a7b6ee0cc2994c95027009736f2
kanwal gulvi <kanwal.gulvi@gmail.com>
2022-08-05 14:38:44 -0700

    modified for loop

--- index.js
+++ index.js
@@ -94,9 +94,11 @@
         }
 
         // LOOPS
-        for (let i = 0; i<5; i++){
-            console.log("hello world");
+        for (let i = 0; i<=5; i++){
+            if (i % 2 !== 0)
+            console.log(i);
         }
+
 
 
 
